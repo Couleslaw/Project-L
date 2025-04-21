c5d24a4b963097ebebb8422f1fdc1a18d55a4e84
Jakub Smolik <smolik.jakub73@gmail.com>
2025-04-21 14:45:48 +0200

    feat(Unity): add scene transitions

--- Unity/ProjectL/Assets/Scripts/UI/SceneTransitions.cs
+++ Unity/ProjectL/Assets/Scripts/UI/SceneTransitions.cs
@@ -1,23 +1,39 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
+
+#nullable enable
 
 public class SceneTransitions : MonoBehaviour
 {
+    private Image? fadeImage;
+    private Animator? fadeAnimator;
+
+    private void Start()
+    {
+        GameObject? fadeObject = GameObject.FindGameObjectWithTag("Fade");
+        if (fadeObject) {
+            fadeImage = fadeObject.GetComponent<Image>();
+            fadeAnimator = fadeObject.GetComponent<Animator>();
+        }
+    }
+
     public void LoadMainMenu()
     {
         // Load the main menu scene
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        StartCoroutine(FadeOutAndLoadScene("MainMenu"));
     }
 
     public void LoadPlayerSelection()
     {
         // Load the game scene
-        UnityEngine.SceneManagement.SceneManager.LoadScene("PlayerSelection");
+        StartCoroutine(FadeOutAndLoadScene("PlayerSelection"));
     }
 
     public void LoadGame()
     {
         // Load the game scene
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+        StartCoroutine(FadeOutAndLoadScene("Game"));
     }
 
     public void QuitGame()
@@ -31,5 +47,14 @@ public class SceneTransitions : MonoBehaviour
     {
         // Open https://couleslaw.github.io/Project-L/UserDocs/ in the default web browser
         Application.OpenURL("https://couleslaw.github.io/Project-L/UserDocs/");
+    }
+
+    private IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        if (fadeImage != null && fadeAnimator != null) {
+            fadeAnimator.SetBool("Fade", true);
+            yield return new WaitUntil(() => fadeImage.color.a == 1);
+        }
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
