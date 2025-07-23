10f4fb862d045419aef5616ac26d661865da4edc
Jakub Smolik <smolik.jakub73@gmail.com>
2025-07-23 21:53:24 +0200

    chore: deploy new version online

--- docs/play/ServiceWorker.js
+++ docs/play/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Couleslaw-Project L-1.3";
+const cacheName = "Couleslaw-Project L-1.3.1";
 const contentToCache = [
-    "Build/a5d7cfd037d05f8dca37e9e60944b4b7.loader.js",
-    "Build/f20198a4c968120b8efbfdd56f89d11c.framework.js",
-    "Build/50979faea9956853bfeeeb5c1ef6b6e2.data",
-    "Build/60b31a590c8daf13c2cd6b2bb3740fa1.wasm",
+    "Build/4539dde7d79582d6e7b96faf7c1f0e75.loader.js",
+    "Build/39884686707199574f32b541e4aa0f9b.framework.js",
+    "Build/cdd00d073e3fe7589596c76510ef9eef.data",
+    "Build/201a5252ee840946374d1a98a0a0a0cd.wasm",
     "TemplateData/style.css"
 
 ];
